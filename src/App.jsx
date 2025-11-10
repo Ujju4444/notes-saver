e3fbab61ce513644f7bc1c21d29abec1a30097b4
Ujju4444 <ugupta0004@gmail.com>
2025-11-11 00:14:04 +0530

    fix: router basename + gh pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,57 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Home from "./components/Home"
-import Paste from "./components/Paste"
-import ViewPaste from "./components/ViewPaste"
-import Navbar from "./components/Navbar"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+import Home from "./components/Home";
+import Paste from "./components/Paste";
+import ViewPaste from "./components/ViewPaste";
+import Navbar from "./components/Navbar";
 
 const router = createBrowserRouter(
   [
     {
-      path:"/",
-      element:
-      <div className="w-full h-full flex flex-col">
-        <Navbar/>
-        <Home/>
-      </div>
+      path: "/",
+      element: (
+        <div className="w-full h-full flex flex-col">
+          <Navbar />
+          <Home />
+        </div>
+      ),
     },
     {
-      path:"/pastes",
-      element: <div className="w-full h-full flex flex-col">
-      <Navbar/>
-      <Paste/>
-    </div>
+      path: "/pastes",
+      element: (
+        <div className="w-full h-full flex flex-col">
+          <Navbar />
+          <Paste />
+        </div>
+      ),
     },
     {
-      path:"/pastes/:id",
-      element: <div className="w-full h-full flex flex-col">
-      <Navbar/>
-      <ViewPaste/>
-    </div>,
-    }
-  ]
-)
+      path: "/pastes/:id",
+      element: (
+        <div className="w-full h-full flex flex-col">
+          <Navbar />
+          <ViewPaste />
+        </div>
+      ),
+    },
+    {
+      path: "*",
+      element: (
+        <div className="w-full h-full flex flex-col">
+          <Navbar />
+          <Home />
+        </div>
+      ),
+    },
+  ],
+  {
+    basename: "/notes-saver", // THIS FIXES GitHub Pages routing
+  }
+);
 
-function App() {
-
-  return (
-    <RouterProvider router={router}/>
-  )
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App
